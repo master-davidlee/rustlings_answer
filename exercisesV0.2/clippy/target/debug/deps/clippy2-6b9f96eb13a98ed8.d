ddc7453917c875171ef5e31fcda75ea09556bd4b
master-davidlee <18920396056@163.com>
2021-02-04 10:46:43 +0800

    20210204

---
+++ exercisesV0.2/clippy/target/debug/deps/clippy2-6b9f96eb13a98ed8.d
@@ -0,0 +1,5 @@
+/home/ipfs/rustlings/exercises/clippy/target/debug/deps/clippy2-6b9f96eb13a98ed8.rmeta: clippy2.rs
+
+/home/ipfs/rustlings/exercises/clippy/target/debug/deps/clippy2-6b9f96eb13a98ed8.d: clippy2.rs
+
+clippy2.rs:
---
+++ exercisesV0.2/clippy/target/rls/debug/deps/clippy2-d2f1db8b22d2ab95.d
@@ -0,0 +1,5 @@
+/home/ipfs/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-d2f1db8b22d2ab95.rmeta: clippy2.rs
+
+/home/ipfs/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-d2f1db8b22d2ab95.d: clippy2.rs
+
+clippy2.rs:
